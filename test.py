ef4a6349cbde4afb063acc041de751f23f8bbe23
GOV MAC 1 <govmac1@GOVs-iMac.local>
2022-03-25 10:56:31 -0300

    small change

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 numbers = [1,2,3,4,5,6,7]
 names = ["fenico","marie","jurmel"]
-verschillende =[1,"marryme,","why",21.50]
+verschillende =[1,"marryme","why",21.50]
 
 for x in numbers:
     print(x)
