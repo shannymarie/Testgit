72dd9102093b160e0aaa3172fdd6f62cb699392b
GOV MAC 1 <govmac1@GOVs-iMac.local>
2022-03-28 09:12:21 -0300

    start

--- test.py
+++ test.py
@@ -2,9 +2,11 @@ numbers = [1,2,3,4,5,6,7]
 names = ["fenico","marie","jurmel"]
 verschillende =[1,"marryme","why",21.50]
 
+print(numbers[:5])
+
 for x in numbers:
     print(x)
-
+    
 for y in names:
     print(y)
 
