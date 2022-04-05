d0e7393fe08aeac938c94daec12b0a9173587558
GOV MAC 1 <fenico10@hotmail.nl>
2022-04-05 15:26:30 -0300

    simple list country code

--- test.py
+++ test.py
@@ -1,3 +1,37 @@
-open("testfile.txt","r").read()
-print(open("testfile.txt","r").read(5))
-print(open("testfile.txt","r").readline())
+Landens = ["suriname","saodi arabie","singapore","senegal"]
+Landena = ["argentinie","algeria","almeria"]
+Landenb = ["belgie","belerus","bulgarie"]
+landenall = []
+landen = []
+
+print("de S landen : ")
+for x in Landens:
+    print(x)
+
+print("de A landen :")
+for y in Landena:
+    print(y)
+
+print("de B landen : ")
+for z in Landenb:
+    print(z)
+
+start = 0 
+while start < 3:
+    land = input("Type 3 landen in : ")
+    landen.append(land)
+    start += 1
+    if start == 3:
+        print("jouw landen zijn : ")
+        for q in landen:
+            print(q)
+
+landenall.extend(Landena)
+landenall.extend(Landenb)
+landenall.extend(Landens)
+landenall.extend(landen)
+
+print("alle landen : ")
+for p in landenall:
+    print(p)
+    
